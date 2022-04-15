2259ebf9b6d436b298cc0d18df9cf5e290bfb44c
MironaGamilMusala <mirona.gamil@musala.com>
2022-04-15 14:11:10 +0200

    adding stylesheets

--- src/main/java/JobBoard/configuration/UserSecurityConfiguration.java
+++ src/main/java/JobBoard/configuration/UserSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -48,6 +49,13 @@ public class UserSecurityConfiguration extends WebSecurityConfigurerAdapter {
         jdbcUserDetailsManager.setDataSource(dataSource);
 
         return jdbcUserDetailsManager;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/resources/**", "/static/**",
+                        "/photos/**","/webjars/**","/css/**");
     }
 
 }
